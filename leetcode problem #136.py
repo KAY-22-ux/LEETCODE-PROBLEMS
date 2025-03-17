681fd19459e4b7860822cccc7d0b47f432d83f52
Kingmystey <calebadjei894@gmail.com>
2025-03-17 16:58:19 +0000

    Initial commit

--- leetcode problem #136.py
+++ leetcode problem #136.py
@@ -1,13 +1,14 @@
 #leetcode problem 136 - Single Number
+
 class Solution(object):
     def singleNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        for i in range(len(nums)):
-            var = nums[0]
-            if nums.count(nums[i]) <= 1 :
-                var = nums[i]
-                return var 
-                                                           
+        var = 0   
+        for i in nums:   
+            var ^= i   
+        return var 
+                                                      
+
