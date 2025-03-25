cd9c05a2c4fdf2b914ec1a0fdda3f0e23ba618e6
Kingmystey <calebadjei894@gmail.com>
2025-03-25 17:13:38 +0000

    update commit

---
+++ leetcode problem #217.py
@@ -0,0 +1,14 @@
+#leetcode problem #217 - Contains Duplicate
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        num_set = set()
+        for i in nums:
+            if i in num_set:
+                return True
+            num_set.add(i)
+        return False
+ 
